b763ed47ab50d8dd56e06bad0509a4a0032f4b32
aisha <achakh81@gmail.com>
2025-07-05 11:50:41 +0300

    Create ProductRepository.java

---
+++ fadhil/fadhil/Market-smart/src/main/java/com/example/Market/smart/Repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.Market.smart.Repository;
+
+import com.example.Market.smart.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
