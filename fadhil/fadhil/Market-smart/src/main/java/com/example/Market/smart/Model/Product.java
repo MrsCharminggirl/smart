d0408872bf2408fd8d994a5e4f6067eea971925c
aisha <achakh81@gmail.com>
2025-07-05 11:50:31 +0300

    Create Product.java

---
+++ fadhil/fadhil/Market-smart/src/main/java/com/example/Market/smart/Model/Product.java
@@ -0,0 +1,75 @@
+package com.example.Market.smart.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+    private String description;
+    private Double price;
+    private int quantity;
+    private String imageUrI;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImageUrI() {
+        return imageUrI;
+    }
+
+    public void setImageUrI(String imageUrI) {
+        this.imageUrI = imageUrI;
+    }
+}
