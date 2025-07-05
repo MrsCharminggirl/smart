a894b884465835c8fb358feefbd4451e1566d13b
aisha <achakh81@gmail.com>
2025-07-05 11:50:50 +0300

    Create ProductServiceImpl.java

---
+++ fadhil/fadhil/Market-smart/src/main/java/com/example/Market/smart/Service/Impl/ProductServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.Market.smart.Service.Impl;
+
+import com.example.Market.smart.Model.Product;
+import com.example.Market.smart.Repository.ProductRepository;
+import com.example.Market.smart.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
