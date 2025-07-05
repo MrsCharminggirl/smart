9eac566bfbbab82a74ad69325554e858c276d1df
aisha <achakh81@gmail.com>
2025-07-05 11:51:12 +0300

    Create MarketSmartApplicationTests.java

---
+++ fadhil/fadhil/Market-smart/src/test/java/com/example/Market/smart/MarketSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Market.smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
