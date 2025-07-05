8f3e5c203992a29aa0476ea4fc8d4f4ec00580a9
aisha <achakh81@gmail.com>
2025-07-05 11:50:25 +0300

    Create MarketSmartApplication.java

---
+++ fadhil/fadhil/Market-smart/src/main/java/com/example/Market/smart/MarketSmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.Market.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSmartApplication.class, args);
+	}
+
+}
