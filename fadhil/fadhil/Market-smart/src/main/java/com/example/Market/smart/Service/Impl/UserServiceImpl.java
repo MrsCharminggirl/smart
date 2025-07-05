96773bec80b8a0e1822efe04891924fe78caf8c4
aisha <achakh81@gmail.com>
2025-07-05 11:50:53 +0300

    Create UserServiceImpl.java

---
+++ fadhil/fadhil/Market-smart/src/main/java/com/example/Market/smart/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.Market.smart.Service.Impl;
+
+import com.example.Market.smart.Model.User;
+import com.example.Market.smart.Repository.UserRepository;
+import com.example.Market.smart.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+
+    @Override
+    public User loginUser(String email, String password) {
+        return userRepository.findByEmail(email)
+                .filter(user -> user.getPassword().equals(password))
+                .orElse(null);
+    }
+}
